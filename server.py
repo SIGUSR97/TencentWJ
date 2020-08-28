bca212ab00760103d1dd672b31b525c4eca0f2d1
jeffzo <jeffzo01@qq.com>
2020-08-28 15:03:37 +0800

    templates

--- server.py
+++ server.py
@@ -1,17 +1,30 @@
 from glob import glob
 
+import bs4
+import requests
+from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(),
+                          chrome_options=options)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+origins = '*'
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,10 +36,20 @@ app.add_middleware(
 
 
 @app.get("/img/{img_name}")
-def get(img_name: str):
+def get_img(img_name: str):
     files = glob(f"./src/assets/{img_name}.*")
     if files:
         path, *_ = files
         return FileResponse(path)
     else:
         pass
+
+
+@app.get("/usercount")
+def get_user_count():
+    driver.get('https://wj.qq.com/')
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, features="html.parser")
+
+    return {"count": int(soup.find(class_='user_count').get_text().replace(',', ''))}
---
+++ test.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(),
+                          chrome_options=options)
+driver.get('https://wj.qq.com/')
+
+html = driver.page_source
+soup = BeautifulSoup(html, features="html.parser")
+
+print("usercount: ", int(soup.find(class_='user_count').get_text().replace(',', '')))
