0b98402d27dceb91931e2a3abb594c9bb7989e07
jeffzo <jeffzo01@qq.com>
2020-09-03 15:30:03 +0800

    test page update

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="test">
-    <h1>This is an test page</h1>
+  <div>
+    <tencent-navbar></tencent-navbar>
+    <div style="padding-top: 100px;">this is a test page</div>
+
+    <copyright-footer></copyright-footer>
   </div>
 </template>
+
+<script>
+import TencentNavbar from '@/components/TencentNavbar.vue';
+import CopyrightFooter from '@/components/CopyrightFooter.vue';
+
+export default {
+  components: {
+    TencentNavbar,
+    CopyrightFooter,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
